98dffe6420f53c7e6fbe721ef6fec6c02e1d6d6b
Nicholas Heuwetter <77307385+nick-heuwetter@users.noreply.github.com>
2024-08-02 05:18:45 -0700

    Fixed for loops being off by one
    The correct number of boxes now displays

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const gridContainer = document.querySelector('#grid-container');
 const sizeBtn = document.querySelector('button')
 
 function makeGrid(num){
-    for(let i = 0; i <= num; i++){
+    for(let i = 0; i < num; i++){
         const row = document.createElement('div');
         row.classList.add('row');
         gridContainer.appendChild(row);
         
-        for(let j = 0; j <= num; j++){
+        for(let j = 0; j < num; j++){
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('grid-square');
             gridSquare.style.height = `${360/num}`+'px'
@@ -25,7 +25,7 @@ function changeSize(){
     let sizeInput = prompt('Pick a size from 1 - 100:');
     let sizeNumber = parseInt(sizeInput);
     if (sizeNumber > 100 || sizeNumber <= 0){
-        alert('INVALID NUMBER')
+        alert('INVALID NUMBER');
     } else {
         gridContainer.innerHTML = '';
         makeGrid(sizeNumber);
